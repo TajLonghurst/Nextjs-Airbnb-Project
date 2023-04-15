80fa999abce30b937591def1f3bb65aea4a135aa
Tajinboxx <TajLonghurstsoft@gmail.com>
2023-04-15 18:52:32 +1200

    Tailwind componets
    Nav

---
+++ app/Components/Navbar/UserMenu.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import React from "react";
+
+const UserMenu = () => {
+  const first = () => {};
+
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3"></div>
+      <div
+        onClick={() => {}}
+        className="hidden md:block text-sm font-semibold py-4 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
+      >
+        Airbnb your home
+      </div>
+    </div>
+  );
+};
+
+export default UserMenu;
