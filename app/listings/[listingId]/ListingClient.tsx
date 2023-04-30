7e1ce3f173f3d13d57c44a037cf3164c6abe2dd0
Tajinboxx <TajLonghurstsoft@gmail.com>
2023-04-30 17:12:15 +1200

    Added Listing componets

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -6,6 +6,8 @@ import { Reservation } from "@prisma/client";
 import { categories } from "@/app/Components/Navbar/Categories";
 import Container from "@/app/Components/Container";
 import ListingHead from "@/app/Components/Listings/ListingHead";
+import { list } from "postcss";
+import ListingInfo from "@/app/Components/Listings/ListingInfo";
 
 interface ListingClientProps {
   resverations?: Reservation[];
@@ -37,6 +39,17 @@ const ListingClient: FC<ListingClientProps> = ({
             id={listing.id}
             currentUser={currentUser}
           />
+          <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+            <ListingInfo
+              user={listing.user}
+              category={category}
+              description={listing.description}
+              roomCount={listing.roomCount}
+              guestCount={listing.guestCount}
+              bathroomCount={listing.guestCount}
+              locationValue={listing.locationValue}
+            />
+          </div>
         </div>
       </div>
     </Container>
